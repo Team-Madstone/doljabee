069c3054bc2b0a4d1e1f9f8410fde5c1d8386041
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2023-01-07 01:58:28 +0900

    ✨ 서버 연결

---
+++ src/server/server.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+
+const PORT = 4000;
+
+const handleListening = () =>
+  console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
+
+app.listen(PORT, handleListening);
