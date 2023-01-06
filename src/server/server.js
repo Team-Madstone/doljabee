aab6eeb2ef173a085e73e5606d6738f566698a76
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2023-01-07 02:07:58 +0900

    🔧 morgan 적용

--- src/server/server.js
+++ src/server/server.js
@@ -1,8 +1,12 @@
 import express from 'express';
-
-const app = express();
+import morgan from 'morgan';
 
 const PORT = 4000;
+
+const app = express();
+const logger = morgan('dev');
+
+app.use(logger);
 
 const handleListening = () =>
   console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
